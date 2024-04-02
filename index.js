3baa94d519783fc2a7adb0fe3e2dd831872850aa
吴晨光 <wuchenguang1998@sina.com>
2024-04-02 17:52:12 +0800

    init:初始化项目

---
+++ index.js
@@ -0,0 +1,46 @@
+// 设置元素显隐
+const toggleHide = (id, isHide) => {
+  const display = isHide ? 'none' : 'block';
+  document.getElementById(id).style.display = display;
+}
+
+let locating = false;
+let lnglat = {};
+let address = '';
+
+// 初始化地图
+const map = new BMapGL.Map("map");
+map.enableScrollWheelZoom(true);
+const point = new BMapGL.Point(116.404, 39.915);
+map.centerAndZoom(point, 15);
+
+// 获取定位
+const getLocation = () => {
+  if (locating) return;
+  toggleHide('loading', false);
+  locating = true;
+  const geolocation = new BMapGL.Geolocation();
+  geolocation.getCurrentPosition(function(r){
+    if (this.getStatus() == BMAP_STATUS_SUCCESS) {
+      map.clearOverlays();
+      const mk = new BMapGL.Marker(r.point);
+      map.addOverlay(mk);
+      map.panTo(r.point);
+      const myGeo = new BMapGL.Geocoder();
+      myGeo.getLocation(r.point, function(result){
+        if (result) {
+          toggleHide('loading', true);
+          lnglat = r.point;
+          address = result.address;
+          document.getElementById('address-text').innerText = address;
+        }
+      });
+    }
+    else {
+      toggleHide('loading', true);
+    }
+    locating = false;
+  });
+}
+
+getLocation();
